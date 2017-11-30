9595109c967b79c46fc3af9747c876baa5a70c4f
Bishwas Shrestha <bishwas.shrestha@metropolia.fi>
2017-11-30 14:28:47 +0200

    first version of the game
    At least one attack mode belly bump. character walk. Animation sprites.

---
+++ Assets/scripts/player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+    public float moveSpeed;
+   // private Animator anim;
+	// Use this for initialization
+	void Start () {
+     //   anim = GetComponent<Animator>();    
+    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxisRaw("Horizontal") > 0.1f || Input.GetAxisRaw("Horizontal") < -0.1f)
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+        }
+
+       // anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+      
+	}
+}
